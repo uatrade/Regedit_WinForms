9ab6d16a00334ba748b55e45b995fc8a5cb09fa2
uatrade <piskovtsev@gmail.com>
2018-10-06 21:27:21 +0300

    Add project files.

---
+++ Regedit_WF/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Regedit_WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnGetList = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtBoxAddName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnGetList
+            // 
+            this.BtnGetList.Location = new System.Drawing.Point(25, 280);
+            this.BtnGetList.Name = "BtnGetList";
+            this.BtnGetList.Size = new System.Drawing.Size(235, 23);
+            this.BtnGetList.TabIndex = 0;
+            this.BtnGetList.Text = "Список автозагрузки";
+            this.BtnGetList.UseVisualStyleBackColor = true;
+            this.BtnGetList.Click += new System.EventHandler(this.BtnGetList_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(278, 251);
+            this.listBox1.TabIndex = 1;
+            
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(28, 82);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(174, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Добавить файл автозагрузку";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtBoxAddName
+            // 
+            this.txtBoxAddName.Location = new System.Drawing.Point(28, 42);
+            this.txtBoxAddName.Name = "txtBoxAddName";
+            this.txtBoxAddName.Size = new System.Drawing.Size(174, 20);
+            this.txtBoxAddName.TabIndex = 3;
+            
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Имя записи";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(25, 320);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(235, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Удалить из автозагрузки";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtBoxAddName);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(306, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(230, 146);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Добавление в атозагрузку";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 355);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.BtnGetList);
+            this.Name = "Form1";
+            this.Text = "Приложение для работы с автозагрузкой";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnGetList;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtBoxAddName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Regedit_WF/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Regedit_WF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            listBox1.MouseDoubleClick += ListBox1_MouseDoubleClick;
+        }
+
+        private void ListBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            RegistryKey currentUser = Registry.CurrentUser;
+            RegistryKey key = currentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
+            string info = key.GetValue(listBox1.SelectedItem.ToString()).ToString();
+
+            MessageBox.Show("Значение ключа реестра\n"+info);
+
+            key.Close();
+        }
+
+        private void BtnGetList_Click(object sender, EventArgs e)
+        {
+            AllRun();      
+        }
+
+        private void AllRun()
+        {
+            listBox1.Items.Clear();
+            try
+            {
+                RegistryKey currentUser = Registry.CurrentUser;
+                RegistryKey key = currentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
+                foreach (var item in key.GetValueNames())
+                {
+                    listBox1.Items.Add(item);
+                }
+                key.Close();
+                currentUser.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            DialogResult res = openFileDialog1.ShowDialog();
+
+            if (res == DialogResult.OK)
+            {
+                RegistryKey currentUser = Registry.CurrentUser;
+                RegistryKey key = currentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+                RegistryKey newkey = key.CreateSubKey(txtBoxAddName.Text);
+                string str = Path.GetFileNameWithoutExtension(openFileDialog1.FileName);
+
+                key.SetValue(str, openFileDialog1.FileName);
+                key.Close();
+                currentUser.Close();
+                AllRun();
+            }
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            RegistryKey currentUser = Registry.CurrentUser;
+            RegistryKey key = currentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+
+            key.DeleteValue(listBox1.SelectedItem.ToString());
+            MessageBox.Show("Запись "+ listBox1.SelectedItem.ToString()+" удалена из реестра");
+            key.Close();
+            currentUser.Close();
+            AllRun();
+        }
+    }
+}
